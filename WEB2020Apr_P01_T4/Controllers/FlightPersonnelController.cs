a94b1b98478eb28f88f029157f977aa992063e7a
Kenneth <kenwong.500@gmail.com>
2020-06-18 18:24:25 +0800

    Halfway through Create Flight Personnel

--- WEB2020Apr_P01_T4/Controllers/FlightPersonnelController.cs
+++ WEB2020Apr_P01_T4/Controllers/FlightPersonnelController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using WEB2020Apr_P01_T4.DAL;
 using WEB2020Apr_P01_T4.Models;
@@ -36,25 +37,48 @@ namespace WEB2020Apr_P01_T4.Controllers
         // GET: FlightPersonnel/Create
         public ActionResult Create()
         {
+            // Stop accessing the action if not logged in       
+            // or account not in the "Staff" role         
+            if ((HttpContext.Session.GetString("Role") == null) || (HttpContext.Session.GetString("Role") != "Staff"))     
+            {             
+                return RedirectToAction("Index", "Home");   
+            }           
+            ViewData["VocationList"] = GetVocation();     
             return View();
         }
 
-        // POST: FlightPersonnel/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+        private List<SelectListItem> GetVocation()
+        { 
+            List<SelectListItem> vocation = new List<SelectListItem>(); 
+            vocation.Add(new SelectListItem { Value = "Administrator,", Text = "Administrator" }); 
+            vocation.Add(new SelectListItem { Value = "Pilot", Text = "Pilot" }); 
+            vocation.Add(new SelectListItem { Value = "Flight Attendance", Text = "Flight Attendance" });
+            return vocation;
         }
+
+
+        // POST: Staff/Create     
+        [HttpPost]    
+        [ValidateAntiForgeryToken]   
+        public ActionResult Create(FlightPersonnel flightPersonnel)   
+        {       
+            //Get country list for drop-down list       
+            //in case of the need to return to Create.cshtml view        
+            ViewData["VocationList"] = GetVocation();       
+            if (ModelState.IsValid)        
+            {                 
+                //Add staff record to database     
+                flightPersonnel.StaffID = staffContext.Add(flightPersonnel);   
+                //Redirect user to Staff/Index view    
+                return RedirectToAction("Index");      
+            }         
+            else     
+            {           
+                //Input validation fails, return to the Create view    
+                //to display error message          
+                return View(flightPersonnel);     
+            }    
+        } 
 
         // GET: FlightPersonnel/Edit/5
         public ActionResult Edit(int id)
--- WEB2020Apr_P01_T4/Models/ValidateEmailExists.cs
+++ WEB2020Apr_P01_T4/Models/ValidateEmailExists.cs
@@ -10,18 +10,22 @@ namespace WEB2020Apr_P01_T4.Models
     public class ValidateEmailExists : ValidationAttribute
     {
         private CustomerDAL customerContext = new CustomerDAL();
+        private FlightPersonnelDAL staffContext = new FlightPersonnelDAL();
 
         protected override ValidationResult IsValid(
             object value, ValidationContext validationContext)
         {
             // Get the email value to validate
             string email = Convert.ToString(value);
-            // Casting the validattion context to the "Customer" model class
+            // Casting the validattion context to the "Customer" and "FlightPersonnel" model class
             Register register = (Register)validationContext.ObjectInstance;
+            FlightPersonnel flightPersonnel = (FlightPersonnel)validationContext.ObjectInstance;
             // Get the Customer Id from the customer instance
             int customerId = register.CustomerID;
+            //Get Staff Id from the Flight Personnel instance
+            int staffId = flightPersonnel.StaffID;
 
-            if (customerContext.IsEmailExist(email, customerId))
+            if (customerContext.IsEmailExist(email, customerId) && (staffContext.IsEmailExist(email, staffId)))
                 // validation failed
                 return new ValidationResult
                     ("Email address already exists!");
