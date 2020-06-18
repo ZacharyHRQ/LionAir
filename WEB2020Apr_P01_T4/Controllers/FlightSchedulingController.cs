b6d25c5348da6c4e9d697550509009ce5a9d4a51
AppDevIn <s10192836@connect.np.edu.sg>
2020-06-19 03:03:40 +0800

    Route with model

--- WEB2020Apr_P01_T4/Controllers/FlightSchedulingController.cs
+++ WEB2020Apr_P01_T4/Controllers/FlightSchedulingController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WEB2020Apr_P01_T4.DAL;
-using WEB2020Apr_P01_T4.Models;
+using WEB2020Apr_P01_T4.ViewModel;
 
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -13,14 +13,21 @@ namespace WEB2020Apr_P01_T4.Controllers
 {
     public class FlightSchedulingController : Controller
     {
+
+
         RouteDAL routeDAL = new RouteDAL();
         FlightScheduleDAL flightScheduleDAL = new FlightScheduleDAL();
         // GET: /<controller>/
         public IActionResult Index()
         {
-            routeDAL.getAllRoutes();
-            List<FlightSchedule> flightSchedules = flightScheduleDAL.getAllFlightSchedule();
-            return View(flightSchedules);
+
+            ScheduleRouteViewModel scheduleRouteViewModel = new ScheduleRouteViewModel
+            {
+                FlightScheduleList = flightScheduleDAL.getAllFlightSchedule(),
+                RouteList = routeDAL.getAllRoutes()
+            };
+            
+            return View(scheduleRouteViewModel);
         }
     }
 }
--- WEB2020Apr_P01_T4/ViewModel/ScheduleRouteViewModel.cs
+++ WEB2020Apr_P01_T4/ViewModel/ScheduleRouteViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using WEB2020Apr_P01_T4.Models;
+
 namespace WEB2020Apr_P01_T4.ViewModel
 {
     public class ScheduleRouteViewModel
     {
-        public ScheduleRouteViewModel()
-        {
-        }
+        public List<FlightSchedule> FlightScheduleList { get; set; }
+        public List<Route> RouteList { get; set; }
     }
 }
