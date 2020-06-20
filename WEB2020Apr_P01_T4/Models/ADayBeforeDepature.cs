9cafbca143375e4541534d614f0a520a0338f26d
AppDevIn <s10192836@connect.np.edu.sg>
2020-06-21 02:39:30 +0800

    Validation for adding Flight

---
+++ WEB2020Apr_P01_T4/Models/ADayBeforeDepature.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace WEB2020Apr_P01_T4.Models
+{
+    public class ADayBeforeDepature : ValidationAttribute
+    {
+        public ADayBeforeDepature()
+        {
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                DateTime datetime = (DateTime)((FlightSchedule)validationContext.ObjectInstance).DepartureDateTime;
+
+                if (DateTime.Now.AddHours(24) <= datetime)
+                    return ValidationResult.Success;
+                else
+                    return new ValidationResult("Departure date must be at least one day after today");
+            }
+            catch(InvalidOperationException i)
+            {
+                return new ValidationResult("The Departure Date Time field is required.");
+            }            
+                
+          
+
+
+        }
+    }
+}
