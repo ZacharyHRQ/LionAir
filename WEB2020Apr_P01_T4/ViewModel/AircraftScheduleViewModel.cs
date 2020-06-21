45ca2ac2a1f4bfa023db2fb8463a8f744785cf29
Primaryheap <s10185319@connect.np.edu.sg>
2020-06-22 02:38:45 +0800

    Change Display Aircraft

--- WEB2020Apr_P01_T4/ViewModel/AircraftScheduleViewModel.cs
+++ WEB2020Apr_P01_T4/ViewModel/AircraftScheduleViewModel.cs
@@ -1,26 +1,18 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using WEB2020Apr_P01_T4.Models;
 namespace WEB2020Apr_P01_T4.ViewModel
 {
     public class AircraftScheduleViewModel
     {
-        public int AircraftID { get; set; }
-        
-        public String AircraftModel { get; set; }
-
-        public int NumEconomySeat { get; set; }
-
-        public int NumBusinessSeat { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime? DateLastMaintenance { get; set; }
-
-        public String FlightSchedule { get; set; }
-
-        public String Status { get; set; }
+        public List<Aircraft> aircraftList { get; set; }
+        public List<FlightSchedule> scheduleList { get; set; }
 
         public AircraftScheduleViewModel()
         {
+            aircraftList = new List<Aircraft>();
+            scheduleList = new List<FlightSchedule>();
         }
     }
 }
