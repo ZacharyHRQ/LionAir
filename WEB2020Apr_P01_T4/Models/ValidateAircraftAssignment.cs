58472b3297745c7575548a999eb16d3da735c7f0
Primaryheap <s10185319@connect.np.edu.sg>
2020-06-28 21:41:54 +0800

    change pages design

---
+++ WEB2020Apr_P01_T4/Models/ValidateAircraftAssignment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WEB2020Apr_P01_T4.Models
+{
+    public class ValidateAircraftAssignment
+    {
+        public ValidateAircraftAssignment()
+        {
+        }
+    }
+}
